2ec27764ffa87ece6c3a9070745d3b2a879dfe1d
tolache <anatoly.cherenkov@gmail.com>
2020-04-30 16:28:21 +0300

    Add two projects

---
+++ My Project 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wow! This is the project 2.");
+        }
+    }
+}
